bc17fe8834f64671afd2150357e451326b2a9066
Tess Lameyer <tjlameyer@gmail.com>
2020-02-14 14:15:49 -0800

    service for fetching a user

---
+++ src/services/gitHubApi.js
@@ -0,0 +1,12 @@
+const request = path => {
+  return fetch(`https://api.github.com/users/${path}`)
+    .then(res => Promise.all([res.ok, res.json()]))
+    .then(([ok, json]) => {
+      if(!ok) throw json;
+
+      return json;
+    });
+};
+
+export const fetchUser = username => request(`/${username}`);
+
